4f785c840563624648a48bc28dd6a2bb3d13fabd
Himangshu Mondal <mailtoherock@gmail.com>
2022-06-26 23:54:11 +0530

    add orbit control

--- app.js
+++ app.js
@@ -6,28 +6,39 @@ const camera = new THREE.PerspectiveCamera(
   0.1,
   1000
 );
+
 const renderer = new THREE.WebGLRenderer({ antialias: true });
 renderer.setSize(window.innerWidth, window.innerHeight);
+// renderer.setClearColor(0xffffff, 0);
 document.body.appendChild(renderer.domElement);
-
+const controls = new THREE.OrbitControls(camera, renderer.domElement);
 // Cube Geometry
 const geometry = new THREE.BoxGeometry(2, 2, 2);
 // color for the cube Geometry
 // const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
 const texture = new THREE.TextureLoader().load('./assets/texture/box.jpg');
 // texture for the cube Geometry
-const material = new THREE.MeshBasicMaterial({ map: texture });
+const material = new THREE.MeshStandardMaterial({ map: texture });
 const cube = new THREE.Mesh(geometry, material);
 scene.add(cube);
 
+const pointLight = new THREE.PointLight(0xffffff);
+pointLight.position.set(5, 0, 5);
+
+const ambientLight = new THREE.AmbientLight(0xffffff);
+scene.add(pointLight, ambientLight);
+
 camera.position.z = 5;
+controls.update();
 
 // Animate the Scene
 function animate() {
   requestAnimationFrame(animate);
-  cube.rotation.x += 0.01;
-  cube.rotation.y += 0.01;
-  cube.rotation.z += 0.01;
+  cube.rotation.x += 0.001;
+  cube.rotation.y += 0.001;
+  cube.rotation.z += 0.001;
+  controls.enablePan = false;
+  controls.update();
   renderer.render(scene, camera);
 }
 
